fc49cb40f0707949262f1c4705b2b487f1fa733e
KindCoffeBear <conter123@list.ru>
2022-01-23 18:16:06 +0300

    Седьмая версия проекта. Добавление и удаление элементов формы

--- js/animation.js
+++ js/animation.js
@@ -19,3 +19,26 @@ function initIncreaseNumberAnimation() {
     increaseNumberAnimationStep(0, element, 5000)
 }
 initIncreaseNumberAnimation();
+
+document.querySelector("#budget").addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+        // Должны добавить еще одно текстовое поле
+        let formContainer = document.createElement('div');
+        formContainer.classList.add('form__group');
+        formContainer.classList.add('form__other-input');
+        
+        let input = document.createElement('input');
+        input.setAttribute("placeholder", "Введите ваш вариант");
+        input.setAttribute("type", "text");
+
+        formContainer.appendChild(input);
+
+        document.querySelector('#form form').insertBefore(formContainer, document.querySelector(".form__submit"));
+    };
+
+    let otherInput = document.querySelector('.form__other-input');
+    if (event.target.value !== 'other' && Boolean(otherInput)) { //в селекте выбрали вариант НЕ Другое И элемент с текстовым полем есть на странице
+        // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+        document.querySelector("#form form").removeChild(otherInput);
+    }
+});
